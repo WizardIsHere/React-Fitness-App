bb3cb4444602cae23595535ec79e696d06e4b8ca
WizardIsHere <mshushant235@gmail.com>
2022-06-15 02:21:50 +0530

    🚧 File and Folder Structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={ <Home />} />
+                <Route path='/exercise/:id' element={ <ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
     )
 }
 
